3305682502a3af73c8b1b7f832613495a2176fd7
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-10-31 22:09:13 +0000

    london-mini-guide-react files added

---
+++ challenge-london-mini-guide/london-mini-guide-react/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import DropDownMenu from './DropDownMenu';
+import CategoryButtons from './CategoryButton';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='header'>London Mini Guide</h1>
+      <DropDownMenu/>
+      <CategoryButtons/>
+    </div>
+  );
+}
+
+export default App;
---
+++ challenge-london-mini-guide/london-mini-guide-react/src/CategoryButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CategoryButtons = () => {
+
+  return (
+    <div>
+        <button type="button" class="btn btn-secondary btn-lg">Pharmacies</button>
+        <button type="button" class="btn btn-secondary btn-lg">schools&colleges</button>
+        <button type="button" class="btn btn-secondary btn-lg">hospitals</button>
+        <button type="button" class="btn btn-secondary btn-lg">doctors</button>
+    </div>
+  );
+};
+
+export default CategoryButtons;
